d971c648adb960e84c1a9396bf778117d1645a8e
Jessie <jrobinson@vt.edu>
2019-10-15 11:07:49 -0400

    Wrote for loops

---
+++ EmergenceUnity/Assets/Scripts/OnOff.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOff : MonoBehaviour {
+
+	public GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+        for(int i = 0; i < 10; i++)
+		{
+            for(int j = 0; j < 10; j++)
+			{
+				Instantiate(cube.transform, new Vector3(i * 2.0f, j * 2.0f, 0), Quaternion.identity);
+			}			
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
